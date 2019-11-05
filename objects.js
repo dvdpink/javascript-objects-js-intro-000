3bd6ee9acaabbad53be3508ed8c7998ace2bae76
David Pink <dvdpink@gmail.com>
2019-11-05 18:15:35 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ var playlist = {
 function updatePlaylist (playlist, artistName, songTitle) {
   playlist[artistName] = "songTitle"
 }
-function removeFromPlaylist (playlist, artistName) {
+function removeFromPlaylist (playlist, artistName) {}
   delete playlist.artistName;
-}
+
